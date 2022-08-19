6e0f04c0afff0a3254ecd048f2b12b5365fbf668
web-pirate <manishsinghdewas@gmail.com>
2022-08-19 21:16:34 +0530

    scaffold, AppBar, Drawer.+Multipaging

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int age = 25;
+    final String name = "Manish";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("$name is $age years old."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
